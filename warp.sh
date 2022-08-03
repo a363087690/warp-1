4996b262f337dac181dc27402ec2e61c30382e81
永雏塔菲 <108621198+taffychan@users.noreply.github.com>
2022-08-03 22:10:43 +0800

    Update warp.sh

--- warp.sh
+++ warp.sh
@@ -778,13 +778,10 @@ warpup(){
     yellow "正在准备刷WARP+流量, 请稍等30秒-1分钟..."
     for ((i = 0; i < ${flowdata}; i++)); do
         [[ $i == 0 ]] && sleep_try=30 && sleep_min=20 && sleep_max=600
-
         install_id=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 22) && \
         curl -X POST -m 10 -sA "okhttp/3.12.1" -H 'content-type: application/json' -H 'Host: api.cloudflareclient.com' \
         --data "{\"key\": \"$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 43)=\",\"install_id\": \"$install_id\",\"fcm_token\": \"APA91b$install_id$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 134)\",\"referrer\": \"$license\",\"warp_enabled\": false,\"tos\": \"$(date -u +%FT%T.$(tr -dc '0-9' </dev/urandom | head -c 3)Z)\",\"type\": \"Android\",\"locale\": \"en_US\"}" \
         --url "https://api.cloudflareclient.com/v0a$(shuf -i 100-999 -n 1)/reg" | grep -qE "referral_count\":1" && status=0 || status=1
-
-        # cloudflare限制了请求频率,目前测试大概在20秒,失败时因延长sleep时间
         [[ $sleep_try > $sleep_max ]] && sleep_try=300
         [[ $sleep_try == $sleep_min ]] && sleep_try=$((sleep_try+1))
         [[ $status == 0 ]] && sleep_try=$((sleep_try-1)) && sleep $sleep_try && rit[i]=$i && echo -n $i-o- && continue
@@ -841,21 +838,16 @@ warpsw1(){
     read -rp "请选择账户类型 [1-3]: " accountInput
     if [[ $accountInput == 1 ]]; then
         wg-quick down wgcf >/dev/null 2>&1
-        
         cd /etc/wireguard
         rm -f wgcf-account.toml
-        
         until [[ -a wgcf-account.toml ]]; do
             yes | wgcf register
             sleep 5
         done
         chmod +x wgcf-account.toml
-        
         wgcf generate
         chmod +x wgcf-profile.conf
-        
         warpsw1_freeplus
-        
         wg-quick up wgcf >/dev/null 2>&1
         yellow "正在检查WARP 免费账户连通性，请稍等..." && sleep 5
         WgcfWARP4Status=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
@@ -875,7 +867,6 @@ warpsw1(){
             done
         fi
         chmod +x wgcf-account.toml
-        
         read -rp "输入WARP账户许可证密钥 (26个字符): " WPPlusKey
         if [[ -n $WPPlusKey ]]; then
             read -rp "请输入自定义设备名，如未输入则使用默认随机设备名: " WPPlusName
@@ -885,14 +876,10 @@ warpsw1(){
             else
                 wgcf update
             fi
-            
             wgcf generate
             chmod +x wgcf-profile.conf
-            
             wg-quick down wgcf >/dev/null 2>&1
-            
             warpsw1_freeplus
-            
             wg-quick up wgcf >/dev/null 2>&1
             yellow "正在检查WARP+账户连通性，请稍等..." && sleep 5
             WgcfWARP4Status=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
@@ -910,12 +897,10 @@ warpsw1(){
         warpsw_teams
         if [[ $wpteamconfirm =~ "y"|"Y" ]]; then
             wg-quick down wgcf >/dev/null 2>&1
-            
             sed -i "s#PublicKey.*#PublicKey = $wpteampublickey#g" /etc/wireguard/wgcf.conf;
             sed -i "s#PrivateKey.*#PrivateKey = $wpteamprivatekey#g" /etc/wireguard/wgcf.conf;
             sed -i "s#Address.*32#Address = $wpteamv4address/32#g" /etc/wireguard/wgcf.conf;
             sed -i "s#Address.*128#Address = $wpteamv6address/128#g" /etc/wireguard/wgcf.conf;
-            
             wg-quick up wgcf >/dev/null 2>&1
             yellow "正在检查WARP Teams账户连通性, 请稍等..."
             WgcfWARP4Status=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
@@ -924,18 +909,13 @@ warpsw1(){
             until [[ $WgcfWARP4Status =~ on|plus ]] || [[ $WgcfWARP6Status =~ on|plus ]]; do
                 red "无法联通WARP Teams账户, 正在尝试重启, 重试次数：$retry_time"
                 retry_time=$((${retry_time} + 1))
-                
                 if [[ $retry_time == 4 ]]; then
                     wg-quick down wgcf >/dev/null 2>&1
-                    
                     cd /etc/wireguard
                     wgcf generate
                     chmod +x wgcf-profile.conf
-                    
                     warpsw1_freeplus
-                    
                     wg-quick up wgcf >/dev/null 2>&1
-                    
                     red "WARP Teams配置有误, 已自动降级至WARP 免费账户 / WARP+"
                 fi
             done
@@ -1028,11 +1008,9 @@ warpsw3(){
         warpsw_teams
         if [[ $wpteamconfirm =~ "y"|"Y" ]]; then
             systemctl stop wireproxy-warp
-            
             sed -i "s#PublicKey.*#PublicKey = $wpteampublickey#g" /etc/wireguard/proxy.conf;
             sed -i "s#PrivateKey.*#PrivateKey = $wpteamprivatekey#g" /etc/wireguard/proxy.conf;
             sed -i "s#Address.*32#Address = $wpteamv4address/32#g" /etc/wireguard/proxy.conf;
-            
             systemctl start wireproxy-warp
             yellow "正在检查WARP Teams账户连通性, 请稍等..."
             WireProxyStatus=$(curl -sx socks5h://localhost:$w5p https://www.cloudflare.com/cdn-cgi/trace -k --connect-timeout 8 | grep warp | cut -d= -f2)
