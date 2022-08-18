e18eb48e088c7da844d71fa9275d3063fe29cf85
taffychan <taffychan@protonmail.com>
2022-08-18 20:06:51 +0800

    Update

--- test/warp.sh
+++ test/warp.sh
@@ -441,10 +441,7 @@ checkwgcf(){
 }
 
 wgcfcheck(){
-    wg-quick down wgcf >/dev/null 2>&1
-    wg-quick up wgcf >/dev/null 2>&1
     yellow "正在启动Wgcf-WARP"
-    checkwgcf
     while [ $i -le 4 ]; do let i++
         wg-quick down wgcf >/dev/null 2>&1
         wg-quick up wgcf >/dev/null 2>&1
