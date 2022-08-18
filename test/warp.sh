d9b47b175838ce6b4ca1e00793d00ed0fc7d0172
taffychan <taffychan@protonmail.com>
2022-08-18 18:44:13 +0800

    Update

--- test/warp.sh
+++ test/warp.sh
@@ -40,3 +40,49 @@ for ((int = 0; int < ${#REGEX[@]}; int++)); do
 done
 
 [[ -z $SYSTEM ]] && red "目前你的VPS的操作系统暂未支持！" && exit 1
+
+menu(){
+    clear
+    echo "#############################################################"
+    echo -e "#                    ${RED} WARP  一键安装脚本${PLAIN}                    #"
+    echo -e "# ${GREEN}作者${PLAIN}: taffychan                                           #"
+    echo -e "# ${GREEN}GitHub${PLAIN}: https://github.com/taffychan                      #"
+    echo "#############################################################"
+    echo -e ""
+    echo -e " ${GREEN}1.${PLAIN} 安装 Wgcf-WARP 全局单栈模式 ${YELLOW}(WARP IPv4)${PLAIN} | ${GREEN}6.${PLAIN} 安装 WARP-Cli 全局模式 ${YELLOW}(WARP IPv4)${PLAIN}"
+    echo -e " ${GREEN}2.${PLAIN} 安装 Wgcf-WARP 全局单栈模式 ${YELLOW}(WARP IPv6)${PLAIN} | ${GREEN}7.${PLAIN} 安装 WARP-Cli 代理模式"
+    echo -e " ${GREEN}3.${PLAIN} 安装 Wgcf-WARP 全局双栈模式             | ${GREEN}8.${PLAIN} 修改 WARP-Cli 代理模式连接端口"
+    echo -e " ${GREEN}4.${PLAIN} 开启或关闭 Wgcf-WARP                    | ${GREEN}9.${PLAIN} 开启或关闭 WARP-Cli 代理模式"
+    echo -e " ${GREEN}5.${PLAIN} ${RED}卸载 Wgcf-WARP${PLAIN}                          | ${GREEN}10.${PLAIN} ${RED}卸载 WARP-Cli${PLAIN}"
+    echo " ----------------------------------------------------------------------------------"
+    echo -e " ${GREEN}11.${PLAIN} 安装 Wireproxy-WARP 代理模式           | ${GREEN}15.${PLAIN} 获取 WARP+ 账户流量"
+    echo -e " ${GREEN}12.${PLAIN} 修改 Wireproxy-WARP 代理模式连接端口   | ${GREEN}16.${PLAIN} 切换 WARP 账户类型"
+    echo -e " ${GREEN}13.${PLAIN} 开启或关闭 Wireproxy-WARP 代理模式     | ${GREEN}17.${PLAIN} 获取解锁 Netflix 的 WARP IP"
+    echo -e " ${GREEN}14.${PLAIN} ${RED}卸载 Wireproxy-WARP 代理模式${PLAIN}           | ${GREEN}0.${PLAIN} 退出脚本"
+    echo -e ""
+    showIP
+    echo -e ""
+    read -rp "请输入选项 [0-17]：" menuChoice
+    case $menuChoice in
+        1) wgcfmode=4 && checkStatus ;;
+        2) wgcfmode=6 && checkStatus ;;
+        3) wgcfmode=5 && checkStatus ;;
+        4) switchWgcf ;;
+        5) uninstallWgcf ;;
+        6) warpcli=2 && installCli ;;
+        7) warpcli=1 && installCli ;;
+        8) warpcli_changeport ;;
+        9) switchCli ;;
+        10) uninstallCli ;;
+        11) installWireProxy ;;
+        12) wireproxy_changeport ;;
+        13) switchWireProxy ;;
+        14) uninstallWireProxy ;;
+        15) warpup ;;
+        16) warpsw ;;
+        17) warpnf ;;
+        *) red "请输入正确的选项 [0-17]！" && exit 1 ;;
+    esac
+}
+
+menu
