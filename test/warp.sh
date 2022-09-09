534503fc5e43e8ca68df5e491be6e42eacfe27b4
taffychan <taffychan@protonmail.com>
2022-09-09 22:02:54 +0800

    Update

--- test/warp.sh
+++ test/warp.sh
@@ -937,6 +937,11 @@ warpsw1(){
             done
         fi
         chmod +x wgcf-account.toml
+        yellow "获取CloudFlare WARP账号密钥信息方法: "
+        green "电脑: 下载并安装CloudFlare WARP→设置→偏好设置→账户→复制密钥到脚本中"
+        green "手机: 下载并安装1.1.1.1 APP→菜单→账户→复制密钥到脚本中"
+        echo ""
+        yellow "重要：请确保手机或电脑的1.1.1.1 APP的账户状态为WARP+！"
         read -rp "输入WARP账户许可证密钥 (26个字符): " warpkey
         until [[ -z $warpkey || $warpkey =~ ^[A-Z0-9a-z]{8}-[A-Z0-9a-z]{8}-[A-Z0-9a-z]{8}$ ]]; do
             red "WARP账户许可证密钥输入错误，请重新输入！"
@@ -945,6 +950,7 @@ warpsw1(){
         if [[ -n $warpkey ]]; then
             sed -i "s/license_key.*/license_key = \"$warpkey\"/g" wgcf-account.toml
             read -rp "请输入自定义设备名，如未输入则使用默认随机设备名: " devicename
+            rm -f wgcf-profile.conf
             green "注册WARP+账户中, 如下方显示:400 Bad Request, 则使用WARP免费版账户"
             if [[ -n $devicename ]]; then
                 wgcf update --name $(echo $devicename | sed s/[[:space:]]/_/g) > /etc/wireguard/info.log 2>&1
@@ -1009,7 +1015,6 @@ warpsw1(){
         yellow "PrivateKey: $wpteamprivatekey"
         yellow "IPv4地址: $wpteamv4address"
         yellow "IPv6地址: $wpteamv6address"
-        echo ""
         read -rp "确认配置信息信息正确请输入y, 其他按键退出升级过程: " wpteamconfirm
         if [[ $wpteamconfirm =~ "y"|"Y" ]]; then
             if [[ -n $(type -P wgcf) && -n $(type -P wg-quick) ]]; then
@@ -1096,6 +1101,11 @@ warpsw1(){
 warpsw2(){
     warp-cli --accept-tos disconnect >/dev/null 2>&1
     warp-cli --accept-tos register >/dev/null 2>&1
+    yellow "获取CloudFlare WARP账号密钥信息方法: "
+    green "电脑: 下载并安装CloudFlare WARP→设置→偏好设置→账户→复制密钥到脚本中"
+    green "手机: 下载并安装1.1.1.1 APP→菜单→账户→复制密钥到脚本中"
+    echo ""
+    yellow "重要：请确保手机或电脑的1.1.1.1 APP的账户状态为WARP+！"
     read -rp "输入WARP账户许可证密钥 (26个字符): " warpkey
     until [[ -z $warpkey || $warpkey =~ ^[A-Z0-9a-z]{8}-[A-Z0-9a-z]{8}-[A-Z0-9a-z]{8}$ ]]; do
         red "WARP账户许可证密钥输入错误，请重新输入！"
